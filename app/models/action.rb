ee633a53ae3ca5389d1de366abe6a07109082a02
Kenley Arai <me@kenleyarai.com>
2017-11-12 14:36:54 -0800

    Fixed initial tests

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,3 +1,7 @@
 class Action < ApplicationRecord
+  # Associations
   belongs_to :project
+
+  # Validations
+  validates_presence_of :title
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+    # Associations
+    has_many :actions, dependent: :destroy
+    # Validations
+    validates_presence_of :title
 end
