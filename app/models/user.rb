18fd08a473b6d2e4bc2efb0375c9aaaa79ad8ebf
Kenley Arai <me@kenleyarai.com>
2017-11-12 21:38:46 -0800

    User tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    # Associations
+    has_many :projects, foreign_key: :created_by
+
+    # Validations
+    validates_presence_of :name, :email, :password_digest
 end
